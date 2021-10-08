54506329f82c8e6c94106ed7486df80c0df99033
Saidur Rahman Saeed <68588036+saeed921@users.noreply.github.com>
2021-10-08 22:47:07 +0600

    Add files via upload

---
+++ Uri1095.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uri1095;
+
+/**
+ *
+ * @author SaeedZ
+ */
+public class Uri1095 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int j,i;
+        for(j=1,i=60;i>=0;j=j+3,i=i-5){
+         
+             
+            //sum=j+3;
+            //s=n*i;
+              //sum=sum+3;
+            System.out.printf("I=%d J=%d\n", j,i );
+        
+        }
+        
+        
+    }
+    
+}
